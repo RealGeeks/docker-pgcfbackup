85fe06f069bad5aaf463e1b371d91a09dc2bf4aa
Kevin McCarthy <me@kevinmccarthy.org>
2015-07-01 11:24:13 -1000

    run pg_dumpall with --clean so the generated dump will include commands to clean out the old stuff

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@
 NOW=$(date +"%Y-%m-%d")
 FILE=$(hostname)-$NOW-$(date +"%H-%M-%S").sql.gz
 
-pg_dumpall > $FILE
+pg_dumpall --clean > $FILE
 
 swift -A https://auth.api.rackspacecloud.com/v1.0 -U $CLOUDFILES_USERNAME -K $CLOUDFILES_APIKEY upload $CLOUDFILES_CONTAINER $FILE
